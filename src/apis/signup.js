e20b7e660f3f6f303815a97eb66a17c03d5df79d
Venkat Kumar (External - TaTa Consultancy Services Sverige AB) <venkat.kumar4@ikea.com>
2023-11-23 22:48:40 +0100

    cros origin error

--- src/apis/signup.js
+++ src/apis/signup.js
@@ -5,6 +5,7 @@ const getSignup = async (req, res) => {
 
     res.status(200).json({
         statusMessage: "Success",
+        email:req.email
     });
 };
 
--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const routerV1 = require("./router");
+const cors = require('cors')
+
 
 class App {
   constructor() {
     this.app = express();
   }
   start() {
+    this.app.use(cors());
     this._attachRouter();
     this.app.listen(8080);
     console.log("server running at port " + (8080));
   }
   _attachRouter() {
+    
     this.app.use("/lactosafe/v1", routerV1);
   }
 
